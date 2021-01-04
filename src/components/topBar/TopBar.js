a0f500ecb978ec335e3e268f32a5317a8f29b578
Darius Placzek <dariusplaczek@gmail.com>
2021-01-04 19:50:52 +0100

    added date function

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import Date from './Date';
+import Date from '../function/UseDate';
 import './topbar.css';
 
 function TopBar() {
+
+    const {gameDay, gameMonth, gameYear} = Date();
+
     return (
         <div className="topbar-container">
-            <h4><Date /></h4>
+            <h4>{`${gameDay} - ${gameMonth} - ${gameYear}`}</h4>
         </div>
     )
 }
