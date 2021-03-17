de1d3479d1632506b636daf0f9eab4d0b4e7d32a
dariusplaczek <dariusplaczek@gmail.com>
2021-03-17 18:36:07 +0100

    added new player and change schema

--- backend/schemas/firstSchema.js
+++ backend/schemas/firstSchema.js
@@ -1,8 +1,54 @@
 import { v4 as uuidv4 } from 'uuid';
 import mongoose from 'mongoose'
 
-const uuid = uuidv4()
 const Schema = mongoose.Schema;
+
+
+const product = new Schema({
+    _id: Number,
+    name: String,
+    count: Number,
+    price: Number,
+    cycle: Number
+})
+
+// const playerWare = new Schema(
+//     {
+//         id: {
+//             type: String,
+//             default: "01"
+//         },
+//         name: {
+//             type: String,
+//             default: "Gerste"
+//         },
+//         count: {
+//             type: Number,
+//             default: 100
+//         },
+//         cycle: {
+//             type: Number,
+//             default: 12
+//         }
+//     }, {
+//         id: {
+//             type: String,
+//             default: "02"
+//         },
+//         name: {
+//             type: String,
+//             default: "Roggen"
+//         },
+//         count: {
+//             type: Number,
+//             default: 80
+//         },
+//         cycle: {
+//             type: Number,
+//             default: 14
+//         }
+//     }
+// )
 
 const firstSchema = new Schema({
     name: String,
@@ -14,14 +60,14 @@ const firstSchema = new Schema({
     userName: String,
     date: {
         type: Date,
-        default: Date.now,
-      }
+        default: Date.now
+      },
+    warehouse: {
+        playerWarehouse: [product]
+    }
+    
 })
 
-// const firstSchema = mongoose.Schema({
-//     _id: Number,
-//     name: String,
-//     password: String
-// })
+
 
 export default mongoose.model('newUser', firstSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,96 @@
-import userEvent from '@testing-library/user-event';
-import express from 'express'
-import mongoose from 'mongoose'
-import Cors from 'cors'
+import userEvent from "@testing-library/user-event";
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
 
-import NewUser from './schemas/firstSchema.js';
-
+import NewUser from "./schemas/firstSchema.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001;
-const connectionUrl = 'mongodb://localhost:27017/test'
+const connectionUrl = "mongodb://localhost:27017/test";
 
 // Middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(Cors());
-
 
 // DB Config
 mongoose.connect(connectionUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
-app.get('/', (req, res) => {
-    res.status(200).send('Send')
+app.get("/", (req, res) => {
+  res.status(200).send("Send");
+});
 
+// Hinzufügen eines Users
+app.post("/addNewUser", (req, res) => {
+  const newUsers = req.body;
+
+  NewUser.create(newUsers, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// Holen eines Users
+app.get("/addNewUser", (req, res) => {
+  NewUser.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+
+// Löschen einer Users
+app.delete("/removeUser", async (req, res) => {
+  try {
+    const removedPost = await NewUser.remove({ _id: "605237d1616dc214580e0996" });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Update eines Users
+app.patch("/updateUser", async (req, res) => {
+  try {
+    const update = await NewUser.updateOne(
+      { _id: '6050e16397cdde9b2129b4b9' },
+      { $set: { name: 'update-User' } }
+      // { _id: req.params.postId },
+      // { $set: { title: req.body.title } }
+    );
+    res.json(update);
+  } catch (err) {
+    res.json({ message: err });
+  }
 })
 
-app.post('/addNewUser', (req, res) => {
-    const newUsers = req.body;
-
-    NewUser.create(newUsers, (err, data) => {
-        if(err) {
-            res.status(500).send(err)
-        } else {
-            res.status(201).send(data)
-        }
-    })
+// Update Aller Users
+app.patch("/allUser", async (req, res) => {
+  try {
+    const update = await NewUser.updateOne(
+      { _id: '6050e16397cdde9b2129b4b9' },
+      { $set: { name: 'updates' } }
+      // { _id: req.params.postId },
+      // { $set: { title: req.body.title } }
+    );
+    res.json(update);
+  } catch (err) {
+    res.json({ message: err });
+  }
 })
 
-app.get('/addNewUser', (req, res) => {
-    NewUser.find((err, data) => {
-        if(err) {
-            res.status(500).send(err)
-        } else {
-            res.status(200).send(data)
-        }
-    })
-})
-
-app.get('/removeUser', (req, res) => {
-    NewUser.remove(err) 
-})
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
