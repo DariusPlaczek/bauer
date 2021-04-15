2d1fc2cd2fcb8567d610c43e44db532e47dc15b3
dariusplaczek <dariusplaczek@gmail.com>
2021-04-15 08:35:13 +0200

    added new line grafik

--- src/Pages/PlayerTrade/PlayerTrade.js
+++ src/Pages/PlayerTrade/PlayerTrade.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import InteriorWarehouseDetail from "../../Components/InteriorWarehouseDetail/InteriorWarehouseDetail";
-import InteriorTradeDetails from "../../Components/InteriorTradeDetails/InteriorTradeDetails"
+import InteriorTradeDetails from "../../Components/InteriorTradeDetails/InteriorTradeDetails";
 import PlayerStock from "../Playerstock/Playerstock";
 import Sellstock from "../Sellstock/Sellstock";
 
@@ -14,7 +14,7 @@ function PlayerTrade() {
 
   useEffect(() => {
     // setTradeList(Object.values(cart))
-    setTradeList(Object.values(cart).filter(value => value.count > 0))
+    setTradeList(Object.values(cart).filter((value) => value.count > 0));
     // const result = Object.values(cart).filter(value => value.count > 0)
     // console.log(result);
   }, [cart]);
@@ -26,7 +26,7 @@ function PlayerTrade() {
         [event[0].id]: {
           productName: event[0].productName,
           count: parseInt(event[0].count),
-          price: event[0].price
+          price: event[0].price,
         },
       });
     } else {
@@ -78,13 +78,24 @@ function PlayerTrade() {
           <div className="quader-out-frame-container player-warehouse">
             <div className="trade-container">
               <div className="col-100">
-               <div className="quader-trader-container">
-                 <button className="button blue"></button>
-
+                <div className="quader-trade-wrapper">
+                  <button className="button blue">Verkaufen</button>
+                  <div className="inner-frame-container trade-inner-frame-container">
+                    <div className="m-pfund-icon">
+                      <h5>999</h5>
+                    </div>
+                    <div className="m-schilling-icon">
+                      <h5>999</h5>
+                    </div>
+                    <div className="m-pfennig-icon">
+                      <h5>999</h5>
+                    </div>
+                  </div>
                 </div>
+                <hr />
               </div>
               <div className="col-100-row">
-              {tradeList &&
+                {tradeList &&
                   tradeList.map((value) => (
                     <InteriorTradeDetails
                       key={`tradePlayer-${value.productName}`}
